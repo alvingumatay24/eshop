652366987d0706e189a19647e2b7e79a3ffe3d98
alvingumatay <alvingumatay13@gmail.com>
2023-09-03 18:23:59 +0800

    updating existing files

--- lib/core/constant/color.dart
+++ lib/core/constant/color.dart
@@ -5,4 +5,6 @@ class AppColor{
   static const Color black =  Color(0xff000000);
   static const Color primaryColor =  Color(0xff5DB1DF);
   static const Color backgroundColor =  Color(0xffffffff);
+  static const Color firstColor =  Color.fromARGB(220, 49, 1, 110);
+  static const Color secondColor =  Color.fromARGB(220, 116, 54, 197);
 }
--- lib/view/screen/home.dart
+++ lib/view/screen/home.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, sort_child_properties_last, sized_box_for_whitespace
 
+import 'package:eshop/core/constant/color.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,13 +9,65 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
      
-        title: Text('HomePage Screen'),
-      ),
       body: Container(
-        child: Center(child: Text('HomePage', style: TextStyle(fontSize: 40),)),
+        child: ListView(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                children: [
+                  Expanded(child: TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      hintText: "Find Product",
+                      hintStyle: TextStyle(fontSize: 18),
+                      border: OutlineInputBorder(
+                       borderSide: BorderSide.none,
+                       borderRadius: BorderRadius.circular(10)
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[200]
+                    ),
+                  )),
+                  SizedBox(width: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                    width: 60,
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    child: IconButton(onPressed: (){}, icon: Icon(Icons.notifications_active_outlined, size: 35,
+                    color: Colors.grey[600],)),)
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              child: Stack( children: [ Container(
+                alignment: Alignment.center,
+                child: ListTile(
+                  title: Text('A Delighted surprise flavors', style: TextStyle(color: Colors.white, fontSize: 20),),
+                  subtitle: Text('100% Chill & Cool!', style: TextStyle(color: Colors.white, fontSize: 30)),
+                ),
+                height: 150,   decoration: BoxDecoration(color: AppColor.secondColor, borderRadius:  BorderRadius.circular(20)),),
+                 Positioned(
+                  top:-20,
+                  right: -50,
+                  child: Container(height: 150, width: 150,decoration: BoxDecoration(
+                    color:AppColor.firstColor,
+                    borderRadius: BorderRadius.circular(160)
+                  ),),
+                )
+                
+                ])),
+               
+          ],
+        )
       ),
+      
     );
   }
 }
